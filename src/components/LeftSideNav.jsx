60f761ef475d35ede04cb7e0d0205414976ccac9
Aslam Hossain <aslamhossain@Aslams-MacBook-Air.local>
2023-12-03 01:52:33 +0600

    requested-work

--- src/components/LeftSideNav.jsx
+++ src/components/LeftSideNav.jsx
@@ -1,26 +1,25 @@
-
 import { useEffect, useState } from "react";
 import LessonName from "../cardComponent/LessonName";
 
 const LeftSideNav = () => {
-    const [lessons, setLessons] = useState([]);
+  const [lessons, setLessons] = useState([]);
 
-    useEffect(()=>{
-        fetch('/fake.json')
-        .then(res => res.json())
-        .then(data => setLessons(data))
-    },[])
-    return (
-        <div className="bg-emerald-900 text-white border-solid border-2 border-sky-500">
-            <div className="border-solid border-2 border-sky-500 text-center">
-            <h3 className="text-xl font-semibold my-2">
-            {
-                    lessons?.map(lesson => <LessonName key={lesson.id} lesson={lesson}></LessonName>)
-                }
-            </h3>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    fetch("/fake.json")
+      .then((res) => res.json())
+      .then((data) => setLessons(data));
+  }, []);
+  return (
+    <div className="">
+      <div className="border-solid border-2 border-sky-500 text-center">
+        <h3 className="text-xl font-semibold my-2">
+          {lessons?.map((lesson) => (
+            <LessonName key={lesson.id} lesson={lesson}></LessonName>
+          ))}
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default LeftSideNav;
--- src/components/MyCourses.jsx
+++ src/components/MyCourses.jsx
@@ -1,10 +1,23 @@
+import { useContext, useEffect, useState } from "react";
 import { Link, useLoaderData, useParams } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
 import LeftSideNav from "./LeftSideNav";
-
 const MyCourses = () => {
   const paras = useLoaderData();
+  const [isAlreadyAvailable, setIsAlreadyAvailable] = useState(false);
   const { id } = useParams();
   const lesson = paras?.find((para) => para.id == id);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    const localData = localStorage.getItem("userInfo");
+    const parseData = JSON.parse(localData);
+    const isAlreadyAvailable = parseData?.lessons?.find(
+      (item) => item.id == id
+    );
+
+    setIsAlreadyAvailable(isAlreadyAvailable);
+  }, [id]);
 
   return (
     <div className="max-w-6xl mx-auto ">
@@ -16,11 +29,36 @@ const MyCourses = () => {
           <h2 className="p-10 text-3xl">{lesson?.para}</h2>
           <h2 className="px-10 py-5 text-3xl">{lesson?.chinese}</h2>
           <h2 className="px-10 py-5 text-3xl">{lesson?.bangla}</h2>
-          
+
           <div className="flex justify-center mt-10 gap-5">
-            <Link to={`/root/task/${id}`}>
-              <div className="btn btn-outline">{lesson?.btn}</div>
-            </Link>
+            {isAlreadyAvailable ? (
+              <button
+                className="btn btn-outline"
+                onClick={() => {
+                  const localData = localStorage.getItem("userInfo");
+                  const parseData = JSON.parse(localData);
+                  const othersLessons = parseData?.lessons?.filter(
+                    (item) => item.id !== id
+                  );
+
+                  const userInfo = {
+                    email: user?.email,
+                    lessons: [...othersLessons],
+                  };
+                  //
+                  localStorage.setItem("userInfo", JSON.stringify(userInfo));
+                  setIsAlreadyAvailable(false);
+                }}
+              >
+                reset
+              </button>
+            ) : (
+              <Link to={`/root/task/${id}`}>
+                <div className="btn btn-outline">{lesson?.btn}</div>
+              </Link>
+            )}
+
+            {lesson?.id && <audio src={lesson?.audio} autoPlay></audio>}
 
             <Link to="/root">
               <button className="btn btn-outline ">{lesson?.btn_back}</button>
